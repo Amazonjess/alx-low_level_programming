8a25c533b968cc6224fc7db119117dbe7ebcdb28
Amazonjess <ikehsummer@gmail.com>
2023-07-17 15:36:28 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: the value in this code is 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
